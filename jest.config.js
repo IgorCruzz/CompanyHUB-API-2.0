b2d20ad225402bc8ec95a36c01f479cadeb321f1
igorcruzz <igorskt2009@gmail.com>
2020-11-23 22:22:55 -0300

    chore: exclude all files

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/**/**protocols**',
+    '!**/test/**.ts',
+    '!<rootDir>/src/presentation/protocols/**'
+  ],
+  coverageDirectory: 'coverage',
+  preset: '@shelf/jest-mongodb',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
