4bd921f7b424104535d36e843b246c1ac8336b6c
igorcruzz <igorskt2009@gmail.com>
2020-11-26 00:05:51 -0300

    feat: ensure UpdateUserController return statusCode 200 if UpdateUser passed on success

--- src/presentation/controllers/user/updateUser.controller.ts
+++ src/presentation/controllers/user/updateUser.controller.ts
@@ -1,7 +1,19 @@
+import { IUpdateUser } from '@/domain/usecases/user/updateUser.interface'
 import { IController, IHttpRequest, IHttpResponse } from '@/presentation/protocols'
 
 export class UpdateUserController implements IController {
+  constructor (
+    private readonly updateUser: IUpdateUser
+  ) {}
+
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    return Promise.resolve(null)
+    const { id } = httpRequest.params
+
+    const updateUser = await this.updateUser.update(id, httpRequest.body)
+
+    return {
+      statusCode: 200,
+      body: { message: 'Atualizado com sucesso.' }
+    }
   }
 }
