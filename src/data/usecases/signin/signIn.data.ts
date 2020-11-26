413a3227d50d007aeab61d929b495b2f4c22ab62
igorcruzz <igorskt2009@gmail.com>
2020-11-26 20:16:18 -0300

    feat: dbSignIn return null with findUserByEmail returns undefined

--- src/data/usecases/signin/signIn.data.ts
+++ src/data/usecases/signin/signIn.data.ts
@@ -1,17 +1,25 @@
+import { IFindUserByEmailRepository } from "@/data/protocols";
 import { ISign } from "@/data/protocols/jwtAdapter/signJwt.interface";
 import { ISignIn, ISignInDTO, ISignInResult } from "@/domain/usecases/signin/signIn.interface";
 
 export class DbSignIn implements ISignIn {
   constructor (
-    private readonly Sign: ISign
+    private readonly Sign: ISign,
+    private readonly findUserByEmailRepo: IFindUserByEmailRepository
   ) {}
 
   async signIn (data: ISignInDTO): Promise<ISignInResult> {
-    return Promise.resolve({
+    const { email, password } = data
+
+    const user = await this.findUserByEmailRepo.findEmail(email)
+
+    if (!user) return null
+
+    return {
       id: 1,
       email: 'user@mail.com',
       name: 'name',
       token: this.Sign.sign(1)
-    })
+    }
   }
 }
