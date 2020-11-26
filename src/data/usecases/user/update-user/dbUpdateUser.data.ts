356427fa1a4f423c49cf440380cfacf8780a25bb
igorcruzz <igorskt2009@gmail.com>
2020-11-25 22:26:33 -0300

    feat: ensure UpdateUser return null if userFindByEmailRepository return an user

--- src/data/usecases/user/update-user/dbUpdateUser.data.ts
+++ src/data/usecases/user/update-user/dbUpdateUser.data.ts
@@ -1,3 +1,4 @@
+import { IFindUserByEmailRepository } from "@/data/protocols";
 import { IFindUserByIdRepository } from "@/data/protocols/db/user/findUserByIdRepository.interface";
 import { IUpdateUserDTO, IUpdateUserRepository } from "@/data/protocols/db/user/updateUserRepository.interface";
 
@@ -5,13 +6,20 @@ import { IUpdateUserDTO, IUpdateUserRepository } from "@/data/protocols/db/user/
 export class DbUpdateUser implements IUpdateUserRepository {
   constructor (
     private readonly findUserByIdRepository: IFindUserByIdRepository,
+    private readonly findUserByEmailRepo: IFindUserByEmailRepository,
+
   ) {}
 
   async update (id: number, data: IUpdateUserDTO): Promise<boolean> {
+    const { email } = data
 
     const user = await this.findUserByIdRepository.findId(id)
 
     if(!user) return null
+
+    if(user.email !== email) {
+      return await this.findUserByEmailRepo.findEmail(email) && null
+    }
 
     return true
   }
