c5e3e72d14a05202f3d58a240d57a79a942e3d52
igorcruzz <igorskt2009@gmail.com>
2020-11-25 22:25:42 -0300

    test: ensure UpdateUser return null if userFindByEmailRepository return an user

--- src/data/usecases/user/update-user/dbUpdateUserData.spec.ts
+++ src/data/usecases/user/update-user/dbUpdateUserData.spec.ts
@@ -1,15 +1,18 @@
+import { IFindUserByEmailRepository } from "@/data/protocols";
 import { IFindUserByIdRepository } from "@/data/protocols/db/user/findUserByIdRepository.interface";
-import { MockUserFindByIdRepository } from "@/data/__mocks__/user.mock";
+import { MockUserFindByEmailRepository, MockUserFindByIdRepository } from "@/data/__mocks__/user.mock";
 import { IUpdateUser } from "@/domain/usecases/user/updateUser.interface"
 import { DbUpdateUser } from "./dbUpdateUser.data";
 
 let updateUserData: IUpdateUser
 let userFindIdRepository: IFindUserByIdRepository
+let userFindByEmailRepository: IFindUserByEmailRepository
 
 describe('UpdateUser Data', () => {
   beforeEach(() => {
     userFindIdRepository = MockUserFindByIdRepository()
-    updateUserData = new DbUpdateUser(userFindIdRepository)
+    userFindByEmailRepository = MockUserFindByEmailRepository()
+    updateUserData = new DbUpdateUser(userFindIdRepository, userFindByEmailRepository)
   })
 
   it('should be defined', () => {
@@ -29,6 +32,20 @@ describe('UpdateUser Data', () => {
     jest.spyOn(userFindIdRepository, 'findId').mockResolvedValue(undefined)
 
     const res = await updateUserData.update(1, { name: 'name' })
+
+    expect(res).toBeNull()
+
+  })
+
+  it('return null if userFindByEmailRepository return an user with email passed on request', async () => {
+    jest.spyOn(userFindByEmailRepository, 'findEmail').mockResolvedValue({
+      id: 1,
+      email: 'other@mail.com',
+      name: 'name',
+      password_hash: 'hashed_password'
+    })
+
+    const res = await updateUserData.update(1, { name: 'name', email: 'other@mail.com' })
 
     expect(res).toBeNull()
 
