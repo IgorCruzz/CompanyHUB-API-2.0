8cf40cda55f29c4f49289d0258998f61a1868acf
igorcruzz <igorskt2009@gmail.com>
2020-11-26 20:15:49 -0300

    test: dbSignIn return null with findUserByEmail returns undefined

--- src/data/usecases/signin/signInData.spec.ts
+++ src/data/usecases/signin/signInData.spec.ts
@@ -1,15 +1,19 @@
 import { MockJwtSignAdapter } from "@/data/mocks/jwt.mock";
+import { MockUserFindByEmailRepository } from "@/data/mocks/user.mock";
+import { IFindUserByEmailRepository } from "@/data/protocols";
 import { ISign } from "@/data/protocols/jwtAdapter/signJwt.interface";
 import { ISignIn } from "@/domain/usecases/signin/signIn.interface"
 import { DbSignIn } from "./signIn.data";
 
 let signInData: ISignIn
 let jwtSignAdapter: ISign
+let userFindByEmailRepository: IFindUserByEmailRepository
 
 describe('SigIn Data', () => {
   beforeEach(() => {
     jwtSignAdapter = MockJwtSignAdapter()
-    signInData = new DbSignIn(jwtSignAdapter)
+    userFindByEmailRepository = MockUserFindByEmailRepository()
+    signInData = new DbSignIn(jwtSignAdapter, userFindByEmailRepository)
   })
 
   it('should be defined', () => {
@@ -17,6 +21,12 @@ describe('SigIn Data', () => {
   })
 
   it('should be able to signin', async () => {
+    jest.spyOn(userFindByEmailRepository, 'findEmail').mockResolvedValue({
+      id: 1,
+      email: 'user@mail.com',
+      name: 'name',
+      password_hash: 'hashed_password'
+    })
 
     const res = await signInData.signIn({
       email: 'user@mail.com',
@@ -33,6 +43,13 @@ describe('SigIn Data', () => {
   })
 
   it('should be able to call Sign with success', async () => {
+    jest.spyOn(userFindByEmailRepository, 'findEmail').mockResolvedValue({
+      id: 1,
+      email: 'user@mail.com',
+      name: 'name',
+      password_hash: 'hashed_password'
+    })
+
     const res = jest.spyOn(jwtSignAdapter, 'sign')
 
     await signInData.signIn({
@@ -41,5 +58,24 @@ describe('SigIn Data', () => {
     })
 
     expect(res).toHaveBeenCalledWith(1)
+  })
+
+  it('should be able to call findUserByEmail with success', async () => {
+    const res =  jest.spyOn(userFindByEmailRepository, 'findEmail')
+
+    await signInData.signIn({
+      email: 'user@mail.com',
+      password: 'password'
+    })
+    expect(res).toHaveBeenCalledWith('user@mail.com')
+  })
+
+  it('return null with findUserByEmail returns undefined', async () => {
+     const res =  await signInData.signIn({
+      email: 'user@mail.com',
+      password: 'password'
+    })
+
+    expect(res).toBeNull()
   })
 });
