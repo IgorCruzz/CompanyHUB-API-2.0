8189f0be52327088222e640e35631106b1fc3d67
igorcruzz <igorskt2009@gmail.com>
2020-11-25 21:59:47 -0300

    test: updateUser should be able to call usersRepository with success

--- src/data/usecases/user/update-user/dbUpdateUserData.spec.ts
+++ src/data/usecases/user/update-user/dbUpdateUserData.spec.ts
@@ -1,15 +1,28 @@
+import { IFindUserByIdRepository } from "@/data/protocols/db/user/findUserByIdRepository.interface";
+import { MockUserFindByIdRepository } from "@/data/__mocks__/user.mock";
 import { IUpdateUser } from "@/domain/usecases/user/updateUser.interface"
 import { DbUpdateUser } from "./dbUpdateUser.data";
 
 let updateUserData: IUpdateUser
+let userFindIdRepository: IFindUserByIdRepository
 
 describe('UpdateUser Data', () => {
   beforeEach(() => {
-    updateUserData = new DbUpdateUser()
+    userFindIdRepository = MockUserFindByIdRepository()
+    updateUserData = new DbUpdateUser(userFindIdRepository)
   })
 
   it('should be defined', () => {
     expect(updateUserData).toBeDefined()
+  })
+
+  it('should be able to call usersRepository with success', async () => {
+
+    const res = jest.spyOn(userFindIdRepository, 'findId')
+
+    await updateUserData.update(1, { name: 'name' })
+
+   expect(res).toHaveBeenCalledWith(1)
   })
 
 });
