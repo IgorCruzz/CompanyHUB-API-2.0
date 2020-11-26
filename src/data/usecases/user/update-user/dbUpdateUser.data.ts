b3b71a364caae301a878b4e14218302583198d31
igorcruzz <igorskt2009@gmail.com>
2020-11-25 22:00:55 -0300

    feat: updateUser should be able to call usersRepository with success

--- src/data/usecases/user/update-user/dbUpdateUser.data.ts
+++ src/data/usecases/user/update-user/dbUpdateUser.data.ts
@@ -1,8 +1,16 @@
+import { IFindUserByIdRepository } from "@/data/protocols/db/user/findUserByIdRepository.interface";
 import { IUpdateUserDTO, IUpdateUserRepository } from "@/data/protocols/db/user/updateUserRepository.interface";
 
 
 export class DbUpdateUser implements IUpdateUserRepository {
+  constructor (
+    private readonly findUserByIdRepository: IFindUserByIdRepository,
+  ) {}
+
   async update (id: number, data: IUpdateUserDTO): Promise<boolean> {
+
+    const user = await this.findUserByIdRepository.findId(id)
+
     return Promise.resolve(null)
   }
 
