ecd13f5ac31f298b71d5ab38ddcce1c9a0ef0bce
igorcruzz <igorskt2009@gmail.com>
2020-11-26 19:45:06 -0300

    test: dbSignIn should be able to signin

--- src/data/usecases/signin/signInData.spec.ts
+++ src/data/usecases/signin/signInData.spec.ts
@@ -1,14 +1,34 @@
+import { MockJwtSignAdapter } from "@/data/mocks/jwt.mock";
+import { ISign } from "@/data/protocols/jwtAdapter/signJwt.interface";
 import { ISignIn } from "@/domain/usecases/signin/signIn.interface"
 import { DbSignIn } from "./signIn.data";
 
 let signInData: ISignIn
+let jwtSignAdapter: ISign
 
 describe('SigIn Data', () => {
   beforeEach(() => {
-    signInData = new DbSignIn()
+    jwtSignAdapter = MockJwtSignAdapter()
+    signInData = new DbSignIn(jwtSignAdapter)
   })
 
   it('should be defined', () => {
     expect(signInData).toBeDefined()
+  })
+
+  it('should be able to signin', async () => {
+
+    const res = await signInData.signIn({
+      email: 'user@mail.com',
+      password: 'password'
+    })
+
+    expect(res).toEqual({
+      id: 1,
+      name: 'name',
+      email: 'user@mail.com',
+      token: 'token'
+    })
+
   })
 });
