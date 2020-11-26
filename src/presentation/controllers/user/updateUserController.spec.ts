53c3fa369d498f61e745eeff471b4aeaf2aaf8e2
igorcruzz <igorskt2009@gmail.com>
2020-11-26 00:04:07 -0300

    test: ensure UpdateUserController return statusCode 200 if UpdateUser passed on success

--- src/presentation/controllers/user/updateUserController.spec.ts
+++ src/presentation/controllers/user/updateUserController.spec.ts
@@ -1,5 +1,5 @@
 import { IUpdateUser } from '@/domain/usecases/user/updateUser.interface'
-import { IController } from '@/presentation/protocols'
+import { IController, IHttpRequest } from '@/presentation/protocols'
 import { mockUpdateUser } from '@/presentation/__mocks__/user.mock'
 import { UpdateUserController } from './updateUser.controller'
 
@@ -9,10 +9,26 @@ let updateUserData: IUpdateUser
 describe('UpdateUser Controller', () => {
   beforeEach(() => {
     updateUserData = mockUpdateUser()
-    updateUserController = new UpdateUserController()
+    updateUserController = new UpdateUserController(updateUserData)
   })
 
   it('should be defined', () => {
     expect(updateUserController).toBeDefined()
+  })
+
+  it('return statusCode 200 if UpdateUser passed on success', async () => {
+    const req: IHttpRequest = {
+      params: { id: 1 },
+      body: {
+        name: 'name'
+      }
+    }
+
+    const res = await updateUserController.handle(req)
+
+    expect(res).toEqual({
+      statusCode: 200,
+      body: { message: 'Atualizado com sucesso.' }
+    })
   })
 })
