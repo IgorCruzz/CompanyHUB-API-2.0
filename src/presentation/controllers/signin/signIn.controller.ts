704f7bc4e1537e5997bfd7f0ab65675bb3b561aa
igorcruzz <igorskt2009@gmail.com>
2020-11-26 21:07:38 -0300

    feat: signInController should be call dbSignInData with correct values

--- src/presentation/controllers/signin/signIn.controller.ts
+++ src/presentation/controllers/signin/signIn.controller.ts
@@ -1,10 +1,17 @@
+import { ISignIn } from '@/domain/usecases/signin/signIn.interface'
 import { IController, IHttpRequest, IHttpResponse } from '@/presentation/protocols'
 
 export class SignInController implements IController {
+  constructor (
+    private readonly dbSignInData: ISignIn
+  ) {}
+
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
+    const signIn = await this.dbSignInData.signIn(httpRequest.body)
+
     return Promise.resolve({
       statusCode: 200,
-      body: {}
+      body: signIn
     })
   }
 }
