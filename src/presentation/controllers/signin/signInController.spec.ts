b1502f1f1ab8066020f644096e05809ef0e048d0
igorcruzz <igorskt2009@gmail.com>
2020-11-26 21:06:23 -0300

    test: signInController should be call dbSignInData with correct values

--- src/presentation/controllers/signin/signInController.spec.ts
+++ src/presentation/controllers/signin/signInController.spec.ts
@@ -1,14 +1,57 @@
-import { IController } from '@/presentation/protocols'
+import { ISignIn } from '@/domain/usecases/signin/signIn.interface'
+import { mockDbSignIn } from '@/presentation/mocks/signIn.mock'
+import { IController, IHttpRequest } from '@/presentation/protocols'
 import { SignInController } from './signIn.controller'
 
 let signInController: IController
+let dbSignInData: ISignIn
 
 describe('SignIn Controller', () => {
   beforeEach(() => {
-    signInController = new SignInController()
+    dbSignInData = mockDbSignIn()
+    signInController = new SignInController(dbSignInData)
   })
 
   it('should be defined', () => {
     expect(signInController).toBeDefined()
+  })
+
+  it('should be call dbSignInData with correct values', async () => {
+    const res = jest.spyOn(dbSignInData, 'signIn')
+
+    const req: IHttpRequest = {
+      body: {
+        email: 'user@mail.com',
+        password: 'password'
+      }
+    }
+
+    await signInController.handle(req)
+
+    expect(res).toHaveBeenCalledWith({
+      email: 'user@mail.com',
+      password: 'password'
+    })
+  })
+
+  it('should be returns statusCode 200 if call mockDbSignIn with success', async () => {
+    const req: IHttpRequest = {
+      body: {
+        email: 'user@mail.com',
+        password: 'password'
+      }
+    }
+
+    const res = await signInController.handle(req)
+
+    expect(res).toEqual({
+      statusCode: 200,
+      body: {
+        id: 1,
+        name: 'name',
+        email: 'user@mail.com',
+        token: 'token'
+      }
+    })
   })
 })
