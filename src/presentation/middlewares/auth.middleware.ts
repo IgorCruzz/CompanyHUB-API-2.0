7846cee228cda147df563894500a431c626dc933
igorcruzz <igorskt2009@gmail.com>
2020-11-27 15:25:02 -0300

    feat: ensure dbauthorization should be to call authData with success

--- src/presentation/middlewares/auth.middleware.ts
+++ src/presentation/middlewares/auth.middleware.ts
@@ -1,10 +1,9 @@
-import jwt from 'jsonwebtoken'
 import { IHttpRequest, IHttpResponse, IMiddleware } from '../protocols'
-import { getRepository } from 'typeorm'
-import { User } from '../../infra/db/typeorm/entities/User.entity'
+import { IAuthorization } from '@/domain/usecases/authorization/authorization.interface'
 
 export class AuthMiddleware implements IMiddleware {
   constructor (
+    private readonly dbAuthorization: IAuthorization,
     private readonly role?: string
   ) {}
 
@@ -20,44 +19,6 @@ export class AuthMiddleware implements IMiddleware {
 
     const [, token] = authorization.split(' ')
 
-    const tokenVerify: any = await jwt.verify(token, 'f04af61b3f332afa0ceec786a42cd365', async (err: any, decodedd: any) => {
-      if (decodedd) {
-        httpRequest.userId = decodedd.id
-
-        const user = getRepository(User)
-
-        const verify = await user.findOne({ id: decodedd.id })
-
-        if (!verify) {
-          return {
-            statusCode: 401,
-            body: { message: 'Este token não pertence a nenhum usuário.' }
-          }
-        }
-
-        if (this.role) {
-          if (!verify.administrator && verify.id !== Number(httpRequest.params?.id)) {
-            return {
-              statusCode: 401,
-              body: { message: 'Você não tem permissão para deletar outro usuário.' }
-            }
-          }
-        }
-
-        return {
-          statusCode: 200,
-          body: httpRequest
-        }
-      }
-
-      if (err) {
-        return {
-          statusCode: 401,
-          body: { message: 'Token inválido' }
-        }
-      }
-    })
-
-    return tokenVerify
+    await this.dbAuthorization.auth(token)
   }
 }
