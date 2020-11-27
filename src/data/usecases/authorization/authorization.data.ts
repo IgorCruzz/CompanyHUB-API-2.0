216f33bd8c2b15a9a7d0886b1673de292cefe391
igorcruzz <igorskt2009@gmail.com>
2020-11-27 14:01:41 -0300

    feat: ensure dbauthorization should be able to call VerifyRepository with success

--- src/data/usecases/authorization/authorization.data.ts
+++ src/data/usecases/authorization/authorization.data.ts
@@ -1,7 +1,15 @@
 import { IAuthorization } from "@/domain/usecases/authorization/authorization.interface";
+import { IVerify } from "@/data/protocols/jwtAdapter/verifyJwt.interface";
 
 export class DbAuthorization implements IAuthorization {
+  constructor(
+    private readonly IVerify: IVerify
+  ) {}
+
   async auth (data: any): Promise<any> {
+
+    await this.IVerify.verify(data.token)
+
     return null
   }
 
