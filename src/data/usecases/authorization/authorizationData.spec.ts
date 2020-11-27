302f54c29e9acd303f771f0cf89984b367edcfd8
igorcruzz <igorskt2009@gmail.com>
2020-11-27 14:09:15 -0300

    test: ensure dbauthorization should be able to call VerifyRepository with success

--- src/data/usecases/authorization/authorizationData.spec.ts
+++ src/data/usecases/authorization/authorizationData.spec.ts
@@ -1,15 +1,26 @@
+import { MockJwtVerifyAdapter } from "@/data/mocks/jwt.mock";
+import { IVerify } from "@/data/protocols/jwtAdapter/verifyJwt.interface";
 import { IAuthorization } from "@/domain/usecases/authorization/authorization.interface";
 import { DbAuthorization } from "./authorization.data";
 
 let authorizationData: IAuthorization
+let verifyRepository: IVerify
 
 describe('Authorization Data', () => {
   beforeEach(() => {
-    authorizationData =  new DbAuthorization()
+    verifyRepository = MockJwtVerifyAdapter()
+    authorizationData =  new DbAuthorization(verifyRepository)
   })
 
   it('should be defined', () => {
     expect(authorizationData).toBeDefined()
+  })
 
+  it('should be able to call VerifyRepository with success', async () => {
+    const res = jest.spyOn(verifyRepository, 'verify')
+
+    await authorizationData.auth({ token: 'token'})
+
+    expect(res).toHaveBeenCalledWith('token')
   })
 });
