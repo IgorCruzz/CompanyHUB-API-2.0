3eb79ee79461fa0bb310b7c0a39755d0b73a1a61
igorcruzz <igorskt2009@gmail.com>
2020-11-27 15:50:52 -0300

    feat: ensure authMiddleware should return statusCode 500 if authData throws

--- src/presentation/middlewares/auth.middleware.ts
+++ src/presentation/middlewares/auth.middleware.ts
@@ -4,21 +4,43 @@ import { IAuthorization } from '@/domain/usecases/authorization/authorization.in
 export class AuthMiddleware implements IMiddleware {
   constructor (
     private readonly dbAuthorization: IAuthorization,
-    private readonly role?: string
+    private readonly role: boolean
   ) {}
 
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    const { authorization } = httpRequest.headers
+    try {
+      const { authorization } = httpRequest.headers
 
-    if (!authorization) {
+      if (!authorization) {
+        return {
+          statusCode: 401,
+          body: { message: 'Insira o token.' }
+        }
+      }
+
+      const [, token] = authorization.split(' ')
+
+      const authUser = await this.dbAuthorization.auth({
+        token,
+        role: this.role
+      })
+
+      if (authUser.error) {
+        return {
+          statusCode: 401,
+          body: { message: authUser.error }
+        }
+      }
+
       return {
-        statusCode: 401,
-        body: { message: 'Insira o token.' }
+        statusCode: 200,
+        body: authUser
+      }
+    } catch (err) {
+      return {
+        statusCode: 500,
+        body: err
       }
     }
-
-    const [, token] = authorization.split(' ')
-
-    await this.dbAuthorization.auth(token)
   }
 }
