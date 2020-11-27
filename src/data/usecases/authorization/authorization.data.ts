0e1f97206feed928ed5aa3ee502ad066c8fbe88d
igorcruzz <igorskt2009@gmail.com>
2020-11-27 14:38:08 -0300

    feat: ensure dbauthorization should be able to call userFindIdRepository with success

--- src/data/usecases/authorization/authorization.data.ts
+++ src/data/usecases/authorization/authorization.data.ts
@@ -1,9 +1,11 @@
 import { IAuthorization } from "@/domain/usecases/authorization/authorization.interface";
 import { IVerify } from "@/data/protocols/jwtAdapter/verifyJwt.interface";
+import { IFindUserByIdRepository } from "@/data/protocols/db/user/findUserByIdRepository.interface";
 
 export class DbAuthorization implements IAuthorization {
   constructor(
-    private readonly Verify: IVerify
+    private readonly Verify: IVerify,
+    private readonly findUserByIdRepository: IFindUserByIdRepository,
   ) {}
 
   async auth (data: any): Promise<any> {
@@ -11,6 +13,8 @@ export class DbAuthorization implements IAuthorization {
     const decoded = await this.Verify.verify(data.token)
 
     if(!decoded) return { error: 'Token inválido.' }
+
+    const user = await this.findUserByIdRepository.findId(decoded.id)
 
     return decoded
   }
