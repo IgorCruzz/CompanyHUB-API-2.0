d0b83f7783f7fb5e6777c9e54c95a815ade4ca36
igorcruzz <igorskt2009@gmail.com>
2020-11-28 12:39:01 -0300

    feat: create yup validation to addCompany

---
+++ src/infra/yup/addCompany.validator.ts
@@ -0,0 +1,25 @@
+import { IValidation, IValidationResult } from '@/data/protocols/yup/validation.interface'
+import * as Yup from 'yup'
+
+export class UpdateUserValidation implements IValidation {
+  async validate (data: any): Promise<IValidationResult> {
+    const schema = Yup.object().shape({
+      name: Yup.string()
+        .min(5)
+        .max(100)
+        .required(),
+      cnpj: Yup.string().required()
+    })
+
+    return await schema.validate(data.body, { abortEarly: false }).then(() => {
+      return { validate: true }
+    }).catch(err => {
+      if (err) {
+        return {
+          validate: false,
+          err
+        }
+      }
+    })
+  }
+}
