4c22f6c9d710faf0436edc8754ac746aa1d63230
igorcruzz <igorskt2009@gmail.com>
2020-11-28 12:09:48 -0300

    test: ensure createCompanyController  should return statusCode 200 if DbAddCompany returns a Company

--- src/presentation/controllers/company/createCompanyController.spec.ts
+++ src/presentation/controllers/company/createCompanyController.spec.ts
@@ -1,14 +1,40 @@
-import { IController } from '@/presentation/protocols'
+import { IAddCompany } from '@/domain/usecases/company/addCompany.interface'
+import { DbAddCompanyStub } from '@/presentation/mocks/company.mock'
+import { IController, IHttpRequest } from '@/presentation/protocols'
 import { CreateCompanyController } from './createCompany.controller'
 
 let createCompanyController: IController
+let addCompany: IAddCompany
 
 describe('CreateCompany Conroller', () => {
   beforeEach(() => {
-    createCompanyController = new CreateCompanyController()
+    addCompany = new DbAddCompanyStub()
+    createCompanyController = new CreateCompanyController(addCompany)
   })
 
   it('should be defined', () => {
     expect(createCompanyController).toBeDefined()
+  })
+
+  it('should return statusCode 200 if DbAddCompany returns a Company', async () => {
+    const req: IHttpRequest = {
+      userId: '1',
+      body: {
+        name: 'company',
+        cnpj: '1111111111'
+      }
+    }
+
+    const res = await createCompanyController.handle(req)
+
+    expect(res).toEqual({
+      statusCode: 200,
+      body: {
+        id: 1,
+        user_id: 1,
+        name: 'company',
+        cnpj: '1111111111'
+      }
+    })
   })
 })
