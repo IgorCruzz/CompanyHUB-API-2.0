df8892e6a37008e7459c8a12f86a5a2076ed8c79
igorcruzz <igorskt2009@gmail.com>
2020-11-29 13:00:56 -0300

    feat: dbDeleteCompany should be able to call userFindIdRepository with success

--- src/data/usecases/company/deleteCompany/dbDeleteCompany.data.ts
+++ src/data/usecases/company/deleteCompany/dbDeleteCompany.data.ts
@@ -1,17 +1,24 @@
 import { IFindByIdRepository } from "@/data/protocols/db/company/findByIdRepository.interface";
+import { IFindUserByIdRepository } from "@/data/protocols/db/user/findUserByIdRepository.interface";
 import { IDbDeleteCompany, IDbDeleteCompanyDTO, IDbDeleteCompanyResult } from "@/domain/usecases/company/deleteCompany.interface";
 
 export class DbDeleteCompany implements IDbDeleteCompany {
   constructor (
-    private readonly findByIdRepository: IFindByIdRepository
+    private readonly findByIdRepository: IFindByIdRepository,
+    private readonly findUserByIdRepository: IFindUserByIdRepository,
+
   ) {}
 
   async delete (data: IDbDeleteCompanyDTO): Promise<IDbDeleteCompanyResult> {
-    const { user } = data
+    const { user, params } = data
 
-    const userId = Number(user)
+    const findCompany = await this.findByIdRepository.findId(Number(params.id))
 
-    const companyExists = await this.findByIdRepository.findId(userId)
+    if (!findCompany) return { error: 'Não existe uma empresa com este ID.'}
+
+    const findUser = await this.findUserByIdRepository.findId(Number(user))
+
+    if (findUser.id !== findCompany.user_id) return { error: 'Não existe uma empresa com este ID.'}
 
     return null
   }
