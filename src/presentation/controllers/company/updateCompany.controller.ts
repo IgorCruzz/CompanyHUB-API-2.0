c10e9fb4d6c9da7369493a7b8e7a0010aecacf27
igorcruzz <igorskt2009@gmail.com>
2020-11-29 18:12:47 -0300

    feat: updateCompanyController return statusCode 500 if dbUpdateCompany throws

--- src/presentation/controllers/company/updateCompany.controller.ts
+++ src/presentation/controllers/company/updateCompany.controller.ts
@@ -1,7 +1,33 @@
+import { IUpdateCompany } from '@/domain/usecases/company/updateCompany.interace'
 import { IController, IHttpRequest, IHttpResponse } from '@/presentation/protocols'
 
 export class UpdateCompanyController implements IController {
+  constructor (
+    private readonly dbUpdateCompany: IUpdateCompany
+  ) {}
+
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    return await null
+    try {
+      const { id } = httpRequest.params
+
+      const company = await this.dbUpdateCompany.update(id, httpRequest.body)
+
+      if (company.error) {
+        return {
+          statusCode: 400,
+          body: { message: 'Você não cadastrou sua empresa ainda.' }
+        }
+      }
+
+      return {
+        statusCode: 200,
+        body: { message: 'Empresa atualizada com sucesso!.' }
+      }
+    } catch (err) {
+      return {
+        statusCode: 500,
+        body: err
+      }
+    }
   }
 }
