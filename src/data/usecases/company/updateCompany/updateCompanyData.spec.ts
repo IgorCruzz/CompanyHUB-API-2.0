ae889bd292e1a0a83a165a691351bdd49ed36697
igorcruzz <igorskt2009@gmail.com>
2020-11-29 17:41:56 -0300

    test: dbUpdateCompany should call updateCompanyRepository with success

--- src/data/usecases/company/updateCompany/updateCompanyData.spec.ts
+++ src/data/usecases/company/updateCompany/updateCompanyData.spec.ts
@@ -1,15 +1,18 @@
-import { FindOneCompanyRepositoryStub } from "@/data/mocks/company.mock";
+import { FindOneCompanyRepositoryStub, UpdateCompanyRepositoryStub } from "@/data/mocks/company.mock";
 import { IFindOneCompanyRepository } from "@/data/protocols/db/company/findOneCompanyRepository.interface";
+import { IUpdateCompanyRepository } from "@/data/protocols/db/company/updateCompanyRepository.interface";
 import { IUpdateCompany } from "@/domain/usecases/company/updateCompany.interace";
 import { DbUpdateCompany } from "./updateCompany.data";
 
 let dbUpdateCompany: IUpdateCompany
 let findOneCompanyRepository: IFindOneCompanyRepository
+let updateCompanyRepository: IUpdateCompanyRepository
 
 describe('DbUpdateCompany Data', () => {
   beforeEach(() => {
     findOneCompanyRepository = new FindOneCompanyRepositoryStub()
-    dbUpdateCompany = new DbUpdateCompany(findOneCompanyRepository)
+    updateCompanyRepository = new UpdateCompanyRepositoryStub()
+    dbUpdateCompany = new DbUpdateCompany(findOneCompanyRepository, updateCompanyRepository)
   })
 
     it('should be defined', () => {
@@ -50,5 +53,29 @@ describe('DbUpdateCompany Data', () => {
 
       expect(res).toEqual({ error: 'Você não tem permissão parar alterar dados de outra empresa.' })
 
+    })
+
+    it('should call updateCompanyRepository with success', async () => {
+      const res = jest.spyOn(findOneCompanyRepository, 'findOne')
+
+      await dbUpdateCompany.update(1, {
+        name: 'company',
+        cnpj: "111111111",
+        user: "1"
+      })
+
+      expect(res).toHaveBeenCalledWith(1)
+    })
+
+    it('should call updateCompanyRepository with success', async () => {
+      const res = jest.spyOn(updateCompanyRepository, 'update')
+
+      await dbUpdateCompany.update(1, {
+        name: 'company',
+        cnpj: "111111111",
+        user: "1"
+      })
+
+      expect(res).toHaveBeenCalledWith(1, {"cnpj": "111111111", "name": "company", "user": "1"})
     })
 });
