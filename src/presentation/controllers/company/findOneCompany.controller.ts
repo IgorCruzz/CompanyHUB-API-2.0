86b22658c69f164b5b327f16a56b98172e35157b
igorcruzz <igorskt2009@gmail.com>
2020-11-29 16:27:27 -0300

    feat: findOneCompany return statusCode 200 if dbFindOneCompany return true

--- src/presentation/controllers/company/findOneCompany.controller.ts
+++ src/presentation/controllers/company/findOneCompany.controller.ts
@@ -1,7 +1,19 @@
+import { IDbFindOneCompany } from '@/domain/usecases/company/findOneCompany.interface'
 import { IController, IHttpRequest, IHttpResponse } from '@/presentation/protocols'
 
 export class FindOneCompanyController implements IController {
+  constructor (
+    private readonly dbFindOneCompany: IDbFindOneCompany
+  ) {}
+
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    return await null
+    const { userId } = httpRequest
+
+    const company = await this.dbFindOneCompany.findOne(userId)
+
+    return {
+      statusCode: 200,
+      body: company
+    }
   }
 }
