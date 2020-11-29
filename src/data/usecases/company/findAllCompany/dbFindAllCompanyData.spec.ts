ac5658a0c97f9f67aa30c31b2eee9526089f8453
igorcruzz <igorskt2009@gmail.com>
2020-11-29 14:43:15 -0300

    test: ensure DbFindAllCompany should call IFindAllCompaniesRepository with success

--- src/data/usecases/company/findAllCompany/dbFindAllCompanyData.spec.ts
+++ src/data/usecases/company/findAllCompany/dbFindAllCompanyData.spec.ts
@@ -1,16 +1,27 @@
+import { FindAllCompaniesRepositoryStub } from "@/data/mocks/company.mock";
+import { IFindAllCompaniesRepository } from "@/data/protocols/db/company/findAllCompanies.repository";
 import { IDbFindAllCompany } from "@/domain/usecases/company/findAllCompanies.interface";
 import { DbFindAllCompany } from "./dbFindAllCompany.data";
 
-
 let dbFindAllCompany: IDbFindAllCompany
+let findAllCompaniesRepository: IFindAllCompaniesRepository
 
 
 describe('Name of the group', () => {
   beforeEach(() => {
-    dbFindAllCompany = new DbFindAllCompany()
+    findAllCompaniesRepository = new FindAllCompaniesRepositoryStub()
+    dbFindAllCompany = new DbFindAllCompany(findAllCompaniesRepository)
   })
 
   it('should be defined', () => {
     expect(dbFindAllCompany).toBeDefined()
+  })
+
+  it('should call IFindAllCompaniesRepository with success', async () => {
+    const res = jest.spyOn(findAllCompaniesRepository, 'findAll')
+
+    await dbFindAllCompany.findAll()
+
+    expect(res).toHaveBeenCalled()
   })
 });
