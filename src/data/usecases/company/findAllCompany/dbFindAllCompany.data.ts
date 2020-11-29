1a0f73cf4c917a2f97a9a6749e548a19216c165e
igorcruzz <igorskt2009@gmail.com>
2020-11-29 14:44:19 -0300

    feat: ensure DbFindAllCompany should call IFindAllCompaniesRepository with success

--- src/data/usecases/company/findAllCompany/dbFindAllCompany.data.ts
+++ src/data/usecases/company/findAllCompany/dbFindAllCompany.data.ts
@@ -1,8 +1,13 @@
+import { IFindAllCompaniesRepository } from "@/data/protocols/db/company/findAllCompanies.repository";
 import { IDbFindAllCompany, IDbFindAllCompanyResult } from "@/domain/usecases/company/findAllCompanies.interface";
 
 export class DbFindAllCompany implements IDbFindAllCompany {
-   async findAll (): Promise<IDbFindAllCompanyResult> {
-    return null
+  constructor (
+    private readonly findAllCompaniesRepository: IFindAllCompaniesRepository
+  ) {}
+
+   async findAll (): Promise<IDbFindAllCompanyResult[]> {
+    return await this.findAllCompaniesRepository.findAll()
    }
 
 }
