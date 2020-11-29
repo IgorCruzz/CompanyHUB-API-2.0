fb18cbb494b30cbc01d035dbd37b60b3fa2a55ce
igorcruzz <igorskt2009@gmail.com>
2020-11-29 13:53:25 -0300

    test: deleteCompany return statusCode 400 if deleteCompany returns an error

--- src/presentation/controllers/company/deleteCompanyController.spec.ts
+++ src/presentation/controllers/company/deleteCompanyController.spec.ts
@@ -1,18 +1,53 @@
-import { IAddCompany } from '@/domain/usecases/company/addCompany.interface'
-import { DbAddCompanyStub } from '@/presentation/mocks/company.mock'
+
+import { IDbDeleteCompany } from '@/domain/usecases/company/deleteCompany.interface'
+import { DbAddCompanyStub, DbDeleteCompanyStub } from '@/presentation/mocks/company.mock'
 import { IController, IHttpRequest } from '@/presentation/protocols'
 import { DeleteCompanyController } from './deleteCompany.controller'
 
 let deleteCompanyController: IController
-let deleteCompany: IAddCompany
+let deleteCompany: IDbDeleteCompany
 
 describe('DeleteCompany Controller', () => {
   beforeEach(() => {
-    deleteCompany = new DbAddCompanyStub()
-    deleteCompanyController = new DeleteCompanyController()
+    deleteCompany = new DbDeleteCompanyStub()
+    deleteCompanyController = new DeleteCompanyController(deleteCompany)
   })
 
   it('should be defined', () => {
     expect(deleteCompanyController).toBeDefined()
+  })
+
+  it('return statusCode 200 if deleteCompany returns true', async () => {
+    const req: IHttpRequest = {
+      params: {
+        id: 1
+      },
+      userId: '1'
+    }
+
+    const res = await deleteCompanyController.handle(req)
+
+    expect(res).toEqual({
+      statusCode: 200,
+      body: { message: 'Empresa deletada com sucesso!.' }
+    })
+  })
+
+  it('return statusCode 400 if deleteCompany returns an error', async () => {
+    jest.spyOn(deleteCompany, 'delete').mockResolvedValue({ error: 'Não existe uma empresa com este ID.' })
+
+    const req: IHttpRequest = {
+      params: {
+        id: 1
+      },
+      userId: '1'
+    }
+
+    const res = await deleteCompanyController.handle(req)
+
+    expect(res).toEqual({
+      statusCode: 400,
+      body: { message: 'Não existe uma empresa com este ID.' }
+    })
   })
 })
