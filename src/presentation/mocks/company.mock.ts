cf26c1631edb8412c6bc8e11ae4e5a8f054c961c
igorcruzz <igorskt2009@gmail.com>
2020-11-29 16:21:23 -0300

    feat: findOneCompany should be defined

--- src/presentation/mocks/company.mock.ts
+++ src/presentation/mocks/company.mock.ts
@@ -1,6 +1,7 @@
 import { IAddCompany, IAddCompanyDTO, IAddCompanyResult } from '@/domain/usecases/company/addCompany.interface'
 import { IDbDeleteCompany, IDbDeleteCompanyDTO, IDbDeleteCompanyResult } from '@/domain/usecases/company/deleteCompany.interface'
 import { IDbFindAllCompany, IDbFindAllCompanyResult } from '@/domain/usecases/company/findAllCompanies.interface'
+import { IDbFindOneCompany, IDbFindOneCompanyResult } from '@/domain/usecases/company/findOneCompany.interface'
 
 export class DbAddCompanyStub implements IAddCompany {
   async add (data: IAddCompanyDTO): Promise<IAddCompanyResult> {
@@ -36,5 +37,17 @@ export class DbFindAllCompanyStub implements IDbFindAllCompany {
       productConnection: [],
       name: 'company'
     }])
+  }
+}
+
+export class DbFindOneCompanyCompanyStub implements IDbFindOneCompany {
+  async findOne (id: string): Promise<IDbFindOneCompanyResult> {
+    return Promise.resolve({
+      user_id: 1,
+      cnpj: '11111111111',
+      id: 1,
+      productConnection: [],
+      name: 'company'
+    })
   }
 }
