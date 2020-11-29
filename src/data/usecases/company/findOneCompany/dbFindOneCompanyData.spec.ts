8f71f8d7460079b1b8d0716251deb3d9dbdfae67
igorcruzz <igorskt2009@gmail.com>
2020-11-29 15:59:56 -0300

    test: ensure DbFindOneCompany should call findOneCompanyRepository with success

--- src/data/usecases/company/findOneCompany/dbFindOneCompanyData.spec.ts
+++ src/data/usecases/company/findOneCompany/dbFindOneCompanyData.spec.ts
@@ -1,16 +1,28 @@
+import { FindOneCompanyRepositoryStub } from "@/data/mocks/company.mock";
+import { IFindOneCompanyRepository } from "@/data/protocols/db/company/findOneCompanyRepository.interface";
 import { IDbFindOneCompany } from "@/domain/usecases/company/findOneCompany.interface";
 import { DbFindOneCompany } from "./dbFindOneCompany.data";
 
 
 let dbFindOneCompany: IDbFindOneCompany
+let findOneCompanyRepository: IFindOneCompanyRepository
 
 
 describe('DbFindOneCompany Data', () => {
   beforeEach(() => {
-    dbFindOneCompany = new DbFindOneCompany()
+    findOneCompanyRepository = new FindOneCompanyRepositoryStub()
+    dbFindOneCompany = new DbFindOneCompany(findOneCompanyRepository)
   })
 
   it('should be defined', () => {
     expect(dbFindOneCompany).toBeDefined()
+  })
+
+  it('should call findOneCompanyRepository with success', async () => {
+    const res = jest.spyOn(findOneCompanyRepository, 'findOne')
+
+    await dbFindOneCompany.findOne("1")
+
+    expect(res).toHaveBeenCalledWith(1)
   })
 });
