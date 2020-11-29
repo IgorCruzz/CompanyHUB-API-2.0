a0dbe091a4f0d276b5de900087c9827df139da4d
igorcruzz <igorskt2009@gmail.com>
2020-11-29 17:11:56 -0300

    feat: dbUpdateCompany returns an error message if findOneCompanyRepository returns undefined

--- src/data/usecases/company/updateCompany/updateCompany.data.ts
+++ src/data/usecases/company/updateCompany/updateCompany.data.ts
@@ -1,8 +1,17 @@
+import { IFindOneCompanyRepository } from "@/data/protocols/db/company/findOneCompanyRepository.interface";
 import { IUpdateCompany, IUpdateCompanyDTO, IUpdateCompanyResult } from "@/domain/usecases/company/updateCompany.interace";
 
 export class DbUpdateCompany implements IUpdateCompany {
+  constructor (
+    private readonly findOneCompanyRepository: IFindOneCompanyRepository
+  ) {}
 
   async update (id: number, data: IUpdateCompanyDTO): Promise<IUpdateCompanyResult> {
+    const findCompany = await this.findOneCompanyRepository.findOne(Number(id))
+
+    if(!findCompany) return { error: 'Você não cadastrou sua empresa ainda.' }
+
+
     return await null
   }
 }
