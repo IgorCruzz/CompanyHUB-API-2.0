2a60a06071db9cb4bc0f42c5399d3410cd281d59
igorcruzz <igorskt2009@gmail.com>
2020-11-29 16:26:38 -0300

    test: findOneCompany return statusCode 200 if dbFindOneCompany return true

--- src/presentation/controllers/company/findOneCompanyController.spec.ts
+++ src/presentation/controllers/company/findOneCompanyController.spec.ts
@@ -1,18 +1,36 @@
 import { IDbFindOneCompany } from '@/domain/usecases/company/findOneCompany.interface'
 import { DbFindOneCompanyCompanyStub } from '@/presentation/mocks/company.mock'
-import { IController } from '@/presentation/protocols'
+import { IController, IHttpRequest } from '@/presentation/protocols'
 import { FindOneCompanyController } from './findOneCompany.controller'
 
 let findOneCompanyController: IController
-let DbFindOneCompany: IDbFindOneCompany
+let dbFindOneCompany: IDbFindOneCompany
 
 describe('FindOneCompany Controller', () => {
   beforeEach(() => {
-    DbFindOneCompany = new DbFindOneCompanyCompanyStub()
-    findOneCompanyController = new FindOneCompanyController()
+    dbFindOneCompany = new DbFindOneCompanyCompanyStub()
+    findOneCompanyController = new FindOneCompanyController(dbFindOneCompany)
   })
 
   it('should be defined', () => {
     expect(findOneCompanyController).toBeDefined()
+  })
+
+  it('return statusCode 200 if dbFindOneCompany return true', async () => {
+    const req: IHttpRequest = { userId: '1' }
+
+    const res = await findOneCompanyController.handle(req)
+
+    expect(res).toEqual({
+      statusCode: 200,
+      body:
+        {
+          user_id: 1,
+          cnpj: '11111111111',
+          id: 1,
+          productConnection: [],
+          name: 'company'
+        }
+    })
   })
 })
