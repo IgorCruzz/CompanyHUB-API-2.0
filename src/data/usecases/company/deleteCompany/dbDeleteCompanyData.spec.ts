065b25bf1df2e8562b335f5af309ddef38322db7
igorcruzz <igorskt2009@gmail.com>
2020-11-29 12:46:17 -0300

    test: dbDeleteCompany should call findByIdRepository with success

--- src/data/usecases/company/deleteCompany/dbDeleteCompanyData.spec.ts
+++ src/data/usecases/company/deleteCompany/dbDeleteCompanyData.spec.ts
@@ -1,14 +1,29 @@
+import { FindByIdRepositoryStub } from "@/data/mocks/company.mock";
+import { IFindByIdRepository } from "@/data/protocols/db/company/findByIdRepository.interface";
 import { IDbDeleteCompany } from "@/domain/usecases/company/deleteCompany.interface";
 import { DbDeleteCompany } from "./dbDeleteCompany.data";
 
 let dbDeleteCompany: IDbDeleteCompany
+let findByIdRepository: IFindByIdRepository
 
 describe('dbDeleteCompany Data', () => {
   beforeEach(() => {
-    dbDeleteCompany = new DbDeleteCompany()
+    findByIdRepository = new FindByIdRepositoryStub()
+    dbDeleteCompany = new DbDeleteCompany(findByIdRepository)
   })
 
     it('should be defined', () => {
       expect(dbDeleteCompany).toBeDefined()
+    })
+
+    it('should call findByIdRepository with success', async () => {
+      const res = jest.spyOn(findByIdRepository, 'findId')
+
+      await dbDeleteCompany.delete({
+        params: { id: "1" },
+        user: "1"
+      })
+
+      expect(res).toHaveBeenCalledWith(1)
     })
 });
