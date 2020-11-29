bffb4af48833e5a34cbb2d88b385c5e43cc2bdc7
igorcruzz <igorskt2009@gmail.com>
2020-11-29 12:59:47 -0300

    test: dbDeleteCompany should be able to call userFindIdRepository with success

--- src/data/usecases/company/deleteCompany/dbDeleteCompanyData.spec.ts
+++ src/data/usecases/company/deleteCompany/dbDeleteCompanyData.spec.ts
@@ -1,15 +1,19 @@
 import { FindByIdRepositoryStub } from "@/data/mocks/company.mock";
+import { MockUserFindByIdRepository } from "@/data/mocks/user.mock";
 import { IFindByIdRepository } from "@/data/protocols/db/company/findByIdRepository.interface";
+import { IFindUserByIdRepository } from "@/data/protocols/db/user/findUserByIdRepository.interface";
 import { IDbDeleteCompany } from "@/domain/usecases/company/deleteCompany.interface";
 import { DbDeleteCompany } from "./dbDeleteCompany.data";
 
 let dbDeleteCompany: IDbDeleteCompany
 let findByIdRepository: IFindByIdRepository
+let userFindIdRepository: IFindUserByIdRepository
 
 describe('dbDeleteCompany Data', () => {
   beforeEach(() => {
+    userFindIdRepository = MockUserFindByIdRepository()
     findByIdRepository = new FindByIdRepositoryStub()
-    dbDeleteCompany = new DbDeleteCompany(findByIdRepository)
+    dbDeleteCompany = new DbDeleteCompany(findByIdRepository, userFindIdRepository)
   })
 
     it('should be defined', () => {
@@ -26,4 +30,28 @@ describe('dbDeleteCompany Data', () => {
 
       expect(res).toHaveBeenCalledWith(1)
     })
+
+    it('should returns an error message if findByIdRepository return an user', async () => {
+      jest.spyOn(findByIdRepository, 'findId').mockResolvedValue(undefined)
+
+      const res =   await dbDeleteCompany.delete({
+        params: { id: "1" },
+        user: "1"
+      })
+
+      expect(res).toEqual({ error: 'Não existe uma empresa com este ID.'})
+    })
+
+    it('should be able to call userFindIdRepository with success', async () => {
+
+      const res = jest.spyOn(userFindIdRepository, 'findId')
+
+      await dbDeleteCompany.delete({
+        params: { id: "1" },
+        user: "1"
+      })
+
+     expect(res).toHaveBeenCalledWith(1)
+    })
+
 });
