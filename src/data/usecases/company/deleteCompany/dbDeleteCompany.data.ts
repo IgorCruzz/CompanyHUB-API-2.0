b154d89f868891eecde122b57ba417a6d3b56214
igorcruzz <igorskt2009@gmail.com>
2020-11-29 12:47:16 -0300

    feat: dbDeleteCompany should call findByIdRepository with success

--- src/data/usecases/company/deleteCompany/dbDeleteCompany.data.ts
+++ src/data/usecases/company/deleteCompany/dbDeleteCompany.data.ts
@@ -1,7 +1,18 @@
+import { IFindByIdRepository } from "@/data/protocols/db/company/findByIdRepository.interface";
 import { IDbDeleteCompany, IDbDeleteCompanyDTO, IDbDeleteCompanyResult } from "@/domain/usecases/company/deleteCompany.interface";
 
 export class DbDeleteCompany implements IDbDeleteCompany {
+  constructor (
+    private readonly findByIdRepository: IFindByIdRepository
+  ) {}
+
   async delete (data: IDbDeleteCompanyDTO): Promise<IDbDeleteCompanyResult> {
+    const { user } = data
+
+    const userId = Number(user)
+
+    const companyExists = await this.findByIdRepository.findId(userId)
+
     return null
   }
 
