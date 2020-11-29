50e713e5d1fcbc11bbcd298f77d5e0b75b891890
igorcruzz <igorskt2009@gmail.com>
2020-11-29 15:27:34 -0300

    test: ensure DbFindAllCompany return statusCode 500 if DbFindAllCompany throws

--- src/presentation/controllers/company/findAllCompanyController.spec.ts
+++ src/presentation/controllers/company/findAllCompanyController.spec.ts
@@ -10,10 +10,51 @@ let DbFindAllCompany: IDbFindAllCompany
 describe('DeleteCompany Controller', () => {
   beforeEach(() => {
     DbFindAllCompany = new DbFindAllCompanyStub()
-    findAllCompanyController = new FindAllCompanyController()
+    findAllCompanyController = new FindAllCompanyController(DbFindAllCompany)
   })
 
   it('should be defined', () => {
     expect(findAllCompanyController).toBeDefined()
+  })
+
+  it('return statusCode 200 if DbFindAllCompany returns', async () => {
+    const res = await findAllCompanyController.handle({})
+
+    expect(res).toEqual({
+      statusCode: 200,
+      body:
+        [{
+          user_id: 1,
+          cnpj: '11111111111',
+          id: 1,
+          productConnection: [],
+          name: 'company'
+        }, {
+          user_id: 1,
+          cnpj: '11111111111',
+          id: 1,
+          productConnection: [],
+          name: 'company'
+        }]
+
+    })
+  })
+
+  it('return statusCode 500 if DbFindAllCompany throws', async () => {
+    const req: IHttpRequest = {
+      params: {
+        id: 1
+      },
+      userId: '1'
+    }
+
+    jest.spyOn(DbFindAllCompany, 'findAll').mockRejectedValue(new Error())
+
+    const res = await findAllCompanyController.handle({})
+
+    expect(res).toEqual({
+      statusCode: 500,
+      body: new Error()
+    })
   })
 })
