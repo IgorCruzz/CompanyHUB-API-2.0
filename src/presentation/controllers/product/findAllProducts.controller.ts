f4d92f66f77b39c7edb2d070596f6906b158883b
igorcruzz <igorskt2009@gmail.com>
2020-11-30 20:54:56 -0300

    feat: findAllProducts should return statusCode 200 if findAllProduct returns

--- src/presentation/controllers/product/findAllProducts.controller.ts
+++ src/presentation/controllers/product/findAllProducts.controller.ts
@@ -1,7 +1,17 @@
+import { IDbFindAllProducts } from '@/domain/usecases/product/findAllProduct.interface'
 import { IController, IHttpRequest, IHttpResponse } from '@/presentation/protocols'
 
 export class FindAllProductsController implements IController {
+  constructor (
+    private readonly findAllProduct: IDbFindAllProducts
+  ) {}
+
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    return await null
+    const products = await this.findAllProduct.findAll()
+
+    return {
+      statusCode: 200,
+      body: products
+    }
   }
 }
