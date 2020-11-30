76cfa9fa3256d2179d76af264000a4f256b6364e
igorcruzz <igorskt2009@gmail.com>
2020-11-30 14:07:38 -0300

    test: createProductController throw error 500 if addProduct throws

--- src/presentation/controllers/product/_tests_/createProductController.spec.ts
+++ src/presentation/controllers/product/_tests_/createProductController.spec.ts
@@ -36,4 +36,44 @@ describe('CreateProduct Controller', () => {
       }
     })
   })
+
+  it('should be returns statusCode 401 if addProduct returns an error', async () => {
+    jest
+      .spyOn(addProduct, 'add')
+      .mockResolvedValue({ error: 'Você não tem permissão para cadastrar um produto em outra empresa.' })
+
+    const req: IHttpRequest = {
+      userId: '1',
+      body: {
+        name: 'product',
+        company_id: 1
+      }
+    }
+
+    const res = await createProductController.handle(req)
+
+    expect(res).toEqual({
+      statusCode: 401,
+      body: { message: 'Você não tem permissão para cadastrar um produto em outra empresa.' }
+    })
+  })
+
+  it('throw error 500 if addProduct throws', async () => {
+    const req: IHttpRequest = {
+      userId: '1',
+      body: {
+        name: 'product',
+        company_id: 1
+      }
+    }
+
+    jest.spyOn(addProduct, 'add').mockRejectedValue(new Error())
+
+    const res = await createProductController.handle(req)
+
+    expect(res).toEqual({
+      statusCode: 500,
+      body: new Error()
+    })
+  })
 })
