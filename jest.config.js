e71f64f68484ba665d4214454407bb666f800ee0
igorcruzz <igorskt2009@gmail.com>
2020-11-30 13:12:54 -0300

    refactor: change test scripts

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/**/**protocols**',
+    '!**/test/**.ts',
+    '!<rootDir>/src/presentation/protocols/**'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
