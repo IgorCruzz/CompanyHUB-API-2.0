06a9a37be478023464bd9d1e1724943acf94f286
igorcruzz <igorskt2009@gmail.com>
2020-11-30 13:30:20 -0300

    feat: dbAddProduct  should call should call FindByProductNameRepository with success

--- src/data/usecases/product/dbAddProduct.data.ts
+++ src/data/usecases/product/dbAddProduct.data.ts
@@ -1,17 +1,20 @@
 import { IFindUserIdRepository } from "@/data/protocols/db/company/findUserIdRepository.interface";
+import { IFindByProductNameRepository } from "@/data/protocols/db/product/findByNameProductRepository.interface";
 import { IAddProduct, IAddProductDTO, IAddProductResult } from "@/domain/usecases/product/addProductinterface";
 
 export class DbAddProduct implements IAddProduct {
   constructor (
-    private readonly FindUserIdRepository: IFindUserIdRepository
+    private readonly findUserIdRepository: IFindUserIdRepository,
+    private readonly findByProductNameRepository: IFindByProductNameRepository
   ) {}
 
   async add(data: IAddProductDTO): Promise<IAddProductResult> {
 
-    const company = await this.FindUserIdRepository.findUserId(Number(data.user))
+    const company = await this.findUserIdRepository.findUserId(Number(data.user))
 
     if (company.id !== data.company_id) return { error: 'Você não tem permissão para cadastrar um produto em outra empresa.'}
 
+    const productName = await this.findByProductNameRepository.findName(data.name)
 
     return await null
   }
