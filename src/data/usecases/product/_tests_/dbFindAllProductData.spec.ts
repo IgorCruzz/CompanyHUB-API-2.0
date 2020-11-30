d8f8aac0e98190ab37b53bb3f60dde56cf49f6ce
igorcruzz <igorskt2009@gmail.com>
2020-11-30 20:24:34 -0300

    test: dbFindAllProduct should call productFindAllRepository with success

--- src/data/usecases/product/_tests_/dbFindAllProductData.spec.ts
+++ src/data/usecases/product/_tests_/dbFindAllProductData.spec.ts
@@ -1,17 +1,29 @@
+import { FindAllProductsRepositoryStub } from "@/data/mocks/product.mock"
+import { IProductFindAllRepository } from "@/data/protocols/db/product/findAllProductsRepository.interface"
 import { IDbFindAllProducts } from "@/domain/usecases/product/findAllProduct.interface"
 import { DbFindAllProducts } from "../dbFindAllProducts.data"
 
 
 let dbFindAllProducts: IDbFindAllProducts
+let productFindAllRepository: IProductFindAllRepository
 
 
 describe('DbFindAllProduct Data', () => {
   beforeEach(() => {
-    dbFindAllProducts = new DbFindAllProducts()
+    productFindAllRepository = new FindAllProductsRepositoryStub()
+    dbFindAllProducts = new DbFindAllProducts(productFindAllRepository)
   })
 
   it('should be defined', () => {
     expect(dbFindAllProducts).toBeDefined()
+  })
+
+  it('should call productFindAllRepository with success', async () => {
+    const res = jest.spyOn(productFindAllRepository, 'findAll')
+
+    await dbFindAllProducts.findAll()
+
+    expect(res).toHaveBeenCalled()
   })
 
 
