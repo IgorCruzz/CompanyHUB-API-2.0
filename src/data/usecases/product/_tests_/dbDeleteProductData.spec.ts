af512956f415fc2d4bcbb5479de8c703cfb4de9b
igorcruzz <igorskt2009@gmail.com>
2020-11-30 18:55:14 -0300

    test: dbDeleteProduct should return an product deleted

--- src/data/usecases/product/_tests_/dbDeleteProductData.spec.ts
+++ src/data/usecases/product/_tests_/dbDeleteProductData.spec.ts
@@ -1,16 +1,26 @@
 import { FindUserIdRepositorytub } from "@/data/mocks/company.mock"
+import { DeleteProductRepositoryStub, FindByIdRepositoryStub } from "@/data/mocks/product.mock"
 import { IFindUserIdRepository } from "@/data/protocols/db/company/findUserIdRepository.interface"
+import { IDeleteProductRepository } from "@/data/protocols/db/product/deleteProductRepository.interface"
+import { IFindByIdRepository } from "@/data/protocols/db/product/findByIdRepository.interface"
 import { IDbDeleteProduct } from "@/domain/usecases/product/deleteProduct.interface"
 import { DbDeleteProduct } from "../dbDeleteProduct.data"
 
 let dbDeleteProductData: IDbDeleteProduct
 let findUserIdRepository: IFindUserIdRepository
+let findByIdRepository: IFindByIdRepository
+let deleteProductRepository: IDeleteProductRepository
 
 
 describe('DbDeleteProduct Data', () => {
   beforeEach(() => {
     findUserIdRepository = new FindUserIdRepositorytub()
-    dbDeleteProductData = new DbDeleteProduct(findUserIdRepository)
+    findByIdRepository = new FindByIdRepositoryStub()
+    deleteProductRepository = new DeleteProductRepositoryStub()
+    dbDeleteProductData = new DbDeleteProduct(
+      findUserIdRepository,
+      findByIdRepository,
+      deleteProductRepository)
   })
 
   it('should be defined', () => {
@@ -27,4 +37,62 @@ describe('DbDeleteProduct Data', () => {
     })
     expect(res).toHaveBeenCalledWith(1)
   })
+
+  it('should returns an error message if FindUserIdRepository return an user', async () => {
+    jest.spyOn(findUserIdRepository, 'findUserId')
+
+    const res =  await dbDeleteProductData.delete({
+      company_id: 2,
+      params: { id: 2 },
+      user: 1
+    })
+
+    expect(res).toEqual({ error: 'Você não tem permissão para cadastrar um produto em outra empresa.'})
+})
+
+
+it('should call findByIdRepository with success', async () => {
+  const res = jest.spyOn(findByIdRepository, 'findId')
+
+  await dbDeleteProductData.delete({
+    company_id: 1,
+    params: { id: 1 },
+    user: 1
+  })
+  expect(res).toHaveBeenCalledWith(1)
+})
+
+it('should returns an error message if findByIdRepository return an user', async () => {
+  jest.spyOn(findByIdRepository, 'findId').mockResolvedValue(undefined)
+
+  const res =  await dbDeleteProductData.delete({
+    company_id: 1,
+    params: { id: 1 },
+    user: 1
+  })
+
+  expect(res).toEqual({ error: 'Insira um ID valido.'})
+})
+
+it('should call deleteProductRepository with success', async () => {
+  const res = jest.spyOn(findByIdRepository, 'findId')
+
+  await dbDeleteProductData.delete({
+    company_id: 1,
+    params: { id: 1 },
+    user: 1
+  })
+  expect(res).toHaveBeenCalledWith(1)
+})
+
+it('should return an product deleted', async () => {
+
+  const res = await dbDeleteProductData.delete({
+    company_id: 1,
+    params: { id: 1 },
+    user: 1
+  })
+  expect(res).toEqual({ deleted: true })
+})
+
 })
