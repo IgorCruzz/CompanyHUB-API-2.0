f1f25bc37f94d6f9b0fd2642d6799bbeeb158c3a
igorcruzz <igorskt2009@gmail.com>
2020-11-30 20:33:11 -0300

    feat: dbFindAllProduct should return all products

--- src/data/usecases/product/_tests_/dbFindAllProductData.spec.ts
+++ src/data/usecases/product/_tests_/dbFindAllProductData.spec.ts
@@ -2,6 +2,7 @@ import { FindAllProductsRepositoryStub } from "@/data/mocks/product.mock"
 import { IProductFindAllRepository } from "@/data/protocols/db/product/findAllProductsRepository.interface"
 import { IDbFindAllProducts } from "@/domain/usecases/product/findAllProduct.interface"
 import { DbFindAllProducts } from "../dbFindAllProducts.data"
+import MockDate from 'mockdate'
 
 
 let dbFindAllProducts: IDbFindAllProducts
@@ -14,6 +15,14 @@ describe('DbFindAllProduct Data', () => {
     dbFindAllProducts = new DbFindAllProducts(productFindAllRepository)
   })
 
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
   it('should be defined', () => {
     expect(dbFindAllProducts).toBeDefined()
   })
@@ -24,6 +33,28 @@ describe('DbFindAllProduct Data', () => {
     await dbFindAllProducts.findAll()
 
     expect(res).toHaveBeenCalled()
+  })
+
+  it('should return all products', async () => {
+    const res = await dbFindAllProducts.findAll()
+
+    expect(res).toEqual([{
+        id: 1,
+        name: 'product',
+        company_id: 1,
+        companyConnection: {
+          user_id: 1,
+          cnpj: '11111111111',
+          id: 1,
+          productConnection: [],
+          name: 'company',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        serviceConnection: [],
+        created_at: new Date(),
+        updated_at: new Date()
+    }])
   })
 
 
