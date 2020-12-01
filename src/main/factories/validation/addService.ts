ca2f7f49ab0f80e68b838900408bc80ba67635ea
igorcruzz <igorskt2009@gmail.com>
2020-12-01 19:18:18 -0300

    feat: create Route for addService

---
+++ src/main/factories/validation/addService.ts
@@ -0,0 +1,9 @@
+import { AddServiceValidation } from '@/infra/yup/addService.validator'
+import { adapMiddleware } from '../../../main/adapters/expressMiddleware.adapter'
+import { ValidatorDecorator } from '../../../main/decorator/validator.decorator'
+
+export const makeAddServiceValidation = (): any => {
+  const addServiceValidation = new AddServiceValidation()
+  const validation = new ValidatorDecorator(addServiceValidation)
+  return adapMiddleware(validation)
+}
