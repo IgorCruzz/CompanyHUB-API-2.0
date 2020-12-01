86efb928b69fb7ded301db2b239631e4fffd2a26
igorcruzz <igorskt2009@gmail.com>
2020-12-01 18:16:37 -0300

    test: addService  return an error message if product return undefined

--- src/data/usecases/service/_tests_/createServiceData.spec.ts
+++ src/data/usecases/service/_tests_/createServiceData.spec.ts
@@ -1,30 +1,30 @@
-import { FindUserIdRepositorytub } from "@/data/mocks/company.mock";
+import { FindByUserRelationStub, FindUserIdRepositorytub } from "@/data/mocks/company.mock";
 import { FindByProductCompanyIdStub } from "@/data/mocks/product.mock";
-import { IFindUserIdRepository } from "@/data/protocols/db/company/findUserIdRepository.interface";
+import { IFindByUserRelationRepository } from "@/data/protocols/db/company/findByUserRelationRepository";
 import { IFindByProductCompanyId } from "@/data/protocols/db/product/findByProductCompanyIdRepository.interface";
 import { IAddService } from "@/domain/usecases/service/addService.interface";
 import { AddService } from "../createService.data";
 
 
 let addService: IAddService
-let findUserIdRepository: IFindUserIdRepository
+let findByUserRelation: IFindByUserRelationRepository
 let findByProductCompanyId: IFindByProductCompanyId
 
 
 
 describe('AddService Data', () => {
   beforeEach(() => {
-    findUserIdRepository = new FindUserIdRepositorytub()
+    findByUserRelation = new FindByUserRelationStub()
     findByProductCompanyId = new FindByProductCompanyIdStub()
-    addService = new AddService(findUserIdRepository, findByProductCompanyId)
+    addService = new AddService(findByUserRelation, findByProductCompanyId)
   });
 
   it('should be defined', () => {
     expect(addService).toBeDefined()
   })
 
-  it('should call FindUserIdRepository with success', async () => {
-    const res = jest.spyOn(findUserIdRepository, 'findUserId')
+  it('should call findByUserRelation with success', async () => {
+    const res = jest.spyOn(findByUserRelation, 'findByUserRelation')
 
     await addService.add({
       name: 'company',
@@ -50,6 +50,22 @@ describe('AddService Data', () => {
     expect(res).toHaveBeenCalledWith({
       company_id: 1,
       product_id: 1
+    })
+  })
+
+  it('should return an error message if product return undefined and user isnt an administrator', async () => {
+    jest.spyOn(findByProductCompanyId, 'findProductCompanyId').mockResolvedValue(undefined)
+
+
+    const res = await addService.add({
+      name: 'company',
+      description: 'description',
+      user: '1',
+      product_id: 1
+    })
+
+    expect(res).toEqual({
+      error: 'Você não tem permissão para cadastrar um serviço neste produto.'
     })
   })
 })
