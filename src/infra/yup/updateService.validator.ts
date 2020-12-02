391460478fcbab6e55a3f73d9378613a8c046012
igorcruzz <igorskt2009@gmail.com>
2020-12-01 23:24:36 -0300

    feat: create route for Update services

---
+++ src/infra/yup/updateService.validator.ts
@@ -0,0 +1,29 @@
+import {
+  IValidation,
+  IValidationResult
+} from '@/data/protocols/yup/validation.interface'
+import * as Yup from 'yup'
+
+export class UpdateServiceValidation implements IValidation {
+  async validate (data: any): Promise<IValidationResult> {
+    const schema = Yup.object().shape({
+      name: Yup.string()
+        .min(5)
+        .max(50)
+    })
+
+    return await schema
+      .validate(data.body, { abortEarly: false })
+      .then(() => {
+        return { validate: true }
+      })
+      .catch((err) => {
+        if (err) {
+          return {
+            validate: false,
+            err
+          }
+        }
+      })
+  }
+}
