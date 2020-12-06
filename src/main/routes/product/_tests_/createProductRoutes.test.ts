01ad72b9844f6a515ed0698c1439df2a308274b2
igorcruzz <igorskt2009@gmail.com>
2020-12-05 21:02:23 -0300

    feat: create integration tests

---
+++ src/main/routes/product/_tests_/createProductRoutes.test.ts
@@ -0,0 +1,130 @@
+import { connection } from '../../../../infra/db/typeorm/index'
+import app from '../../../config/app'
+import request from 'supertest'
+import { getRepository } from 'typeorm'
+import { User } from '@/infra/db/typeorm/entities/User.entity'
+import { hash } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+import { Company } from '@/infra/db/typeorm/entities/Company.entity'
+import { Product } from '@/infra/db/typeorm/entities/Product.entity'
+
+jest.setTimeout(30000)
+
+describe('Company', () => {
+  beforeAll(async () => {
+    await connection.create()
+  })
+
+  afterAll(async () => {
+    await connection.close()
+  })
+
+  beforeEach(async () => {
+    await getRepository(User).query(`DELETE FROM users`)
+    await getRepository(Company).query(`DELETE FROM companies`)
+    await getRepository(Product).query(`DELETE FROM products`)
+  })
+
+  afterEach(async () => {
+    await getRepository(User).query(`DELETE FROM users`)
+    await getRepository(Company).query(`DELETE FROM companies`)
+    await getRepository(Product).query(`DELETE FROM products`)
+  })
+
+  describe('Create Company', () => {
+    it('POST /products - 201', async () => {
+      const password = await hash('password', 12)
+
+      const user = await getRepository(User).save({
+        name: 'Igor Oliveira da Cruz',
+        email: 'igorcruz.dev@email.com',
+        password_hash: password,
+        activation: true,
+      })
+
+      const company = await getRepository(Company).save({
+        name: 'Company',
+        cnpj: '111111111111',
+        user_id: user.id,
+      })
+
+      const authorization = sign({ id: user.id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRESIN,
+      })
+
+      await request(app)
+        .post('/products')
+        .set('authorization', `Bearer ${authorization}`)
+        .send({
+          name: 'product',
+          company_id: company.id,
+        })
+        .expect(201)
+    })
+
+    it('POST /products - 400', async () => {
+      const password = await hash('password', 12)
+
+      const user = await getRepository(User).save({
+        name: 'Igor Oliveira da Cruz',
+        email: 'igorcruz.dev@email.com',
+        password_hash: password,
+        activation: true,
+      })
+
+      await getRepository(Company).save({
+        name: 'Company',
+        cnpj: '111111111111',
+        user_id: user.id,
+      })
+
+      const authorization = sign({ id: user.id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRESIN,
+      })
+
+      await request(app)
+        .post('/products')
+        .set('authorization', `Bearer ${authorization}`)
+        .send({
+          name: 'product',
+          company_id: 2,
+        })
+        .expect(400)
+    })
+
+    it('POST /products - 400', async () => {
+      const password = await hash('password', 12)
+
+      const user = await getRepository(User).save({
+        name: 'Igor Oliveira da Cruz',
+        email: 'igorcruz.dev@email.com',
+        password_hash: password,
+        activation: true,
+      })
+
+      const company = await getRepository(Company).save({
+        name: 'Company',
+        cnpj: '111111111111',
+        user_id: user.id,
+      })
+
+      await getRepository(Product).save({
+        name: 'product',
+        company_id: company.id,
+      })
+
+      const authorization = sign({ id: user.id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRESIN,
+      })
+
+      await request(app)
+        .post('/products')
+        .set('authorization', `Bearer ${authorization}`)
+        .send({
+          name: 'product',
+          company_id: company.id,
+        })
+        .expect(400)
+    })
+  })
+})
--- src/main/routes/product/_tests_/findAllProductsRoutes.test.ts
+++ src/main/routes/product/_tests_/findAllProductsRoutes.test.ts
@@ -6,10 +6,11 @@ import { User } from '@/infra/db/typeorm/entities/User.entity'
 import { hash } from 'bcrypt'
 import { sign } from 'jsonwebtoken'
 import { Company } from '@/infra/db/typeorm/entities/Company.entity'
+import { Product } from '@/infra/db/typeorm/entities/Product.entity'
 
 jest.setTimeout(30000)
 
-describe('Company', () => {
+describe('Product', () => {
   beforeAll(async () => {
     await connection.create()
   })
@@ -21,15 +22,17 @@ describe('Company', () => {
   beforeEach(async () => {
     await getRepository(User).query(`DELETE FROM users`)
     await getRepository(Company).query(`DELETE FROM companies`)
+    await getRepository(Product).query(`DELETE FROM products`)
   })
 
   afterEach(async () => {
     await getRepository(User).query(`DELETE FROM users`)
     await getRepository(Company).query(`DELETE FROM companies`)
+    await getRepository(Product).query(`DELETE FROM products`)
   })
 
-  describe('FindAll Company', () => {
-    it('GET /companies - 200', async () => {
+  describe('FindAll Product', () => {
+    it('GET /products - 200', async () => {
       const password = await hash('password', 12)
 
       const user = await getRepository(User).save({
@@ -40,10 +43,15 @@ describe('Company', () => {
         administrator: true,
       })
 
-      await getRepository(Company).save({
+      const company = await getRepository(Company).save({
         name: 'Company',
         cnpj: '111111111111',
         user_id: user.id,
+      })
+
+      await getRepository(Product).save({
+        name: 'product',
+        company_id: company.id,
       })
 
       const authorization = sign({ id: user.id }, process.env.JWT_SECRET, {
@@ -51,13 +59,13 @@ describe('Company', () => {
       })
 
       await request(app)
-        .get(`/companies`)
+        .get(`/products`)
         .set('authorization', `Bearer ${authorization}`)
         .expect(200)
     })
 
-    it('GET /companies - 400', async () => {
-      await request(app).get(`/companies`).expect(401)
+    it('GET /products - 401', async () => {
+      await request(app).get(`/products`).expect(401)
     })
   })
 })
